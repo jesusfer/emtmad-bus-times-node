4f12508d8478707a583c1b8b7dec8ab299f16d7d
Jesús Fernández <jesus@nublar.net>
2016-10-22 11:45:53 +0200

    Rewrote the module to use Promises

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-var request = require('request');
+// Copyright (c) 2016 Jesús Fernández <jesus@nublar.net>
+
+'use strict';
+
+const rp = require('request-promise');
+const debug = require('debug')('emtmad-bus-times-promise');
+const _ = require('lodash');
+const P = require('bluebird');
 
 // Probably not the best way to share vars between functions in module scope. Please let me know the right way.
 var globalIdClient = '';
@@ -6,47 +13,66 @@ var globalPasskey = '';
 
 // API variables
 var getIncomingBusesToStopUrl = 'https://openbus.emtmadrid.es/emt-proxy-server/last/geo/GetArriveStop.php';
+var getStopsFromLocationUrl = 'https://openbus.emtmadrid.es/emt-proxy-server/last/geo/GetStopsFromXY.php';
 
 module.exports = {
-	initAPICredentials : function (idClient, passKey ){
-		globalIdClient = idClient;
-		globalPasskey = passKey;
-	},
-	getIncomingBusesToStop : function (idStop, callback ){
+    initAPICredentials: function (idClient, passKey) {
+        globalIdClient = idClient;
+        globalPasskey = passKey;
+    },
+    getIncomingBusesToStop: function (idStop) {
+        debug(`Getting incoming buses at stop ${idStop}`);
+        // Entry parameters
+        var formData = {
+            cultureInfo: "ES",
+            idStop: idStop,
+            idClient: globalIdClient,
+            passKey: globalPasskey
+        };
+        var options = {
+            method: 'POST',
+            uri: getIncomingBusesToStopUrl,
+            form: formData,
+            headers: {
+                /* 'content-type': 'application/x-www-form-urlencoded' */ // Set automatically
+            },
+            json: true // Automatically stringifies the body to JSON
+            // TODO is there an option for not checking the cert?
+        };
 
-		// Response JSON, to be populated later
-		var response = {
-		};
+        return rp(options).then(function (body) {
+            return _.get(body, 'arrives', []);
+        });
+    },
+    getStopsFromLocation: function (location, radius) {
+        debug(`Getting stops in a radius of ${radius}m near ${location.latitude},${location.longitude}`);
+        if ((location.latitude === 0 && location.longitude === 0) || radius < 1) {
+            // There will never be a bus from the EMT here :)
+            debug('Empty location. Not calling the EMT API');
+            return P.resolve([]);
+        }
+        // Entry parameters
+        var formData = {
+            cultureInfo: "ES",
+            idClient: globalIdClient,
+            passKey: globalPasskey,
+            latitude: location.latitude,
+            longitude: location.longitude,
+            Radius: radius
+        };
 
+        var options = {
+            method: 'POST',
+            uri: getStopsFromLocationUrl,
+            form: formData,
+            headers: {
+                /* 'content-type': 'application/x-www-form-urlencoded' */ // Set automatically
+            },
+            json: true // Automatically stringifies the body to JSON
+        };
 
-		// Entry parameters
-		var formData = {
-			"cultureInfo": "ES",
-			"idStop": idStop,
-			"idClient": globalIdClient,
-			"passKey": globalPasskey
-		};
-
-		// Perform API call
-		request.post({
-			url: getIncomingBusesToStopUrl, 
-			form: formData,
-			strictSSL: false // The API certificate looks self signed
-
-		}, function (err, httpResponse, body) {
-			if ( err ) {
-				console.error('Error while connecting:');
-				response.status = 400;
-				response.error = err;
-			}else{
-				console.log('REST call OK');
-				response.status = 200;
-				response.arrives = (JSON.parse(body)).arrives;
-			}
-
-			if (typeof callback === "function") {
-				callback ( response );
-			}
-		});
-	}
-}
+        return rp(options).then(function (body) {
+            return _.get(body, 'stop', []);
+        });
+    }
+};
